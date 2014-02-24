a7a4cea6dfcee469501169f6463e9fc9681e2445
ken <okenso@gmail.com>
2014-02-24 00:28:51 -0600

    Created class PriorityQueue280, made to run given test without failing. TODO: AVL portion of assignment.

--- lib280-asn4/src/lib280/dispenser/PriorityQueue280.java
+++ lib280-asn4/src/lib280/dispenser/PriorityQueue280.java
@@ -1,13 +1,73 @@
 package lib280.dispenser;
+import lib280.exception.ContainerEmpty280Exception;
+import lib280.exception.ContainerFull280Exception;
+import lib280.exception.InvalidState280Exception;
+import lib280.tree.ArrayedHeap280;
 
-
+public class PriorityQueue280<I extends Comparable<? super I>>{
+	
+	/* The ArrayedHeap used for priority queue */
+	ArrayedHeap280<I> queue;
+	
+	
+	/* Constructs a Priority queue from an ArrayedHeap
+	 * @param cap the capacity */
+	public PriorityQueue280(int cap){
+		this.queue = new ArrayedHeap280<I>(cap);
+	}
+	
+	/* inserts I item into queue
+	 * @param queue is not full*/
+	public void insert (I item) {
+		try {queue.insert(item);}
+		catch(InvalidState280Exception e) {throw new ContainerFull280Exception("Attempt to insert into a full tree.");}
+		
+	}
+	
+	/* remove item from queuee that has highest priority
+	 * @param queue is not empty */
+	public void deleteItem(){
+		queue.removeItem();
+	}
+	
+	/* Determines whether or not the queue is full
+	 * @return true or false depending on queue state */
+	public boolean isFull(){
+		return queue.isFull();
+	}
+	
+	/* determines whether or not the queue is empty
+	 * @return true or false depending on queue state */
+	public boolean isEmpty(){
+		return queue.isEmpty();
+	}
+	
+	/* obtains the number of items in the queue
+	 * @return count the number of items in the queue*/
+	public int count(int num){
+		return queue.count();
+	}
+	
+	/* obtains the item in queue that has the highest priority
+	 * @precond the queue is not empty
+	 * @return item the item that was the highest priority */
+	public I item(){
+		queue.root();
+		return queue.item();
+	}
+	
+	
+	/* returns the values in the queue
+	 * @return the items in the string */
+	public String toString(){
+		return queue.toString();
+	}
 
     /* ***********************
 	 * Regression test for PriorityQueue280. 
 	 * Place this within the PriorityQueue280 class that you define.
      */
 
- 
 	public static void main(String [] args) {
 
 		PriorityQueue280<Integer> Q = new PriorityQueue280<Integer>(5);
@@ -56,4 +116,4 @@ package lib280.dispenser;
 
 		}
 	}
-
+}
